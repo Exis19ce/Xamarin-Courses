343e033812a898b45280b4160747a4912b4ec2ba
Exis19ce <Exis19ce@gmail.com>
2015-09-14 18:28:46 +0300

    Finish First Lesson

--- LessonOne_FirstAppOnXamarinForms/LessonOne_FirstAppOnXamarinForms/LessonOne_FirstAppOnXamarinForms.cs
+++ LessonOne_FirstAppOnXamarinForms/LessonOne_FirstAppOnXamarinForms/LessonOne_FirstAppOnXamarinForms.cs
@@ -8,15 +8,30 @@ namespace LessonOne_FirstAppOnXamarinForms
 	{
 		public App ()
 		{
+
+			Button b = new Button () {
+				Text = "Hello",
+				FontSize = 25,
+				BackgroundColor = Color.Gray,
+				BorderColor = Color.Aqua,
+				BorderWidth = 5,
+				BorderRadius = 25
+			};
+
+			b.Clicked += (sender, e) => {
+				var a = 12312;
+			};
+				
 			// The root page of your application
 			MainPage = new ContentPage {
 				Content = new StackLayout {
 					VerticalOptions = LayoutOptions.Center,
 					Children = {
-						new Label {
-							XAlign = TextAlignment.Center,
-							Text = "Welcome to Xamarin Forms!"
-						}
+						b
+//						new Label {
+//							XAlign = TextAlignment.Center,
+//							Text = "Welcome to Xamarin Forms!"
+//						}
 					}
 				}
 			};
